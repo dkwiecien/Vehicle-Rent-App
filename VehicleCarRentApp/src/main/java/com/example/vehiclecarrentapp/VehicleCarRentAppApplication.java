dd2363452d21d66eeaeec9e4effb20e91769c1da
DominikKwiecien0 <242448@edu.p.lodz.pl>
2024-08-26 15:20:21 +0200

    feat: Add Hello World

--- VehicleCarRentApp/src/main/java/com/example/vehiclecarrentapp/VehicleCarRentAppApplication.java
+++ VehicleCarRentApp/src/main/java/com/example/vehiclecarrentapp/VehicleCarRentAppApplication.java
@@ -1,13 +1,9 @@
 package com.example.vehiclecarrentapp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class VehicleCarRentAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VehicleCarRentAppApplication.class, args);
+        System.out.println("Hello World");
     }
 
 }
