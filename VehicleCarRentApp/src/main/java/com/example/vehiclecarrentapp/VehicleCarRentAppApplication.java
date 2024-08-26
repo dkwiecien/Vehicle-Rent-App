73465f1df5da9d911cbae1a320ebfa0f90c7e27b
DominikKwiecien0 <242448@edu.p.lodz.pl>
2024-08-26 15:16:33 +0200

    chore: Add project setup

---
+++ VehicleCarRentApp/src/main/java/com/example/vehiclecarrentapp/VehicleCarRentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehiclecarrentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleCarRentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleCarRentAppApplication.class, args);
+    }
+
+}
---
+++ VehicleCarRentApp/src/test/java/com/example/vehiclecarrentapp/VehicleCarRentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehiclecarrentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleCarRentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
